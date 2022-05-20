7cd6f15e21f14687989e4243dc3599c03a0eb6f6
Thomas <tg85@students.waikato.ac.nz>
2022-05-20 15:26:04 +1200

    Needed reset as failed to compile when going to windows

--- assets/build/service-worker.js
+++ assets/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7daba7c11315f120b1dd2effd4531a2d.js"
+  "/precache-manifest.c4ca35d8fde195dd578b452c4c0f674f.js"
 );
 
 self.addEventListener('message', (event) => {
