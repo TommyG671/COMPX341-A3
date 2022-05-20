7ac49482130dbf9803c1d710bd9d65080a5ca06f
TommyG671 <tg85@students.waikato.ac.nz>
2022-05-20 12:35:15 +1200

    Testing. Currently code doesn't compile. COMPX341-22A-A3 Commiting from CI/CD Pipeline

--- assets/pipeline.ps1
+++ assets/pipeline.ps1
@@ -1,3 +1,12 @@
 npm install
 npm run build
-npm run start
+
+if ($? -eq $false){
+    git add -A
+    git commit -m "Testing. Currently code doesn't compile. COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+    git push COMPX341-A3
+    npm run start
+}
+else {
+    Write-Output "Compile Errors occurred, failed to commit"
+}
